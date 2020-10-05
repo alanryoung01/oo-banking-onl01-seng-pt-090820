afe9b3a0fdb6cce91033b43f5d0868fa2aa19045
Alan Young <alanryoung01@gmail.com>
2020-10-05 19:31:23 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,4 +1,11 @@
 class Transfer
   attr_accessor :status
   attr_reader :amount, :sender, :receiver
+
+  def initialize(sender, receiver, amount)
+    @status = "pending"
+    @sender = sender
+    @receiver = receiver
+    @amount = amount
+  end
 end
