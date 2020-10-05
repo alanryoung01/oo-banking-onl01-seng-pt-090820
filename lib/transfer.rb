264d259914a048753772e3b60b08a23b10acde4a
Alan Young <alanryoung01@gmail.com>
2020-10-05 19:45:51 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -28,8 +28,12 @@ def execute_transaction
   end
 
   def reverse_transfer
-
-
+    if valid? && receiver.balance > amount && self.status == "complete"
+      receiver.balance -= amount
+      sender.balance += amount
+      self.status = "reversed"
+    else
+      reject_transfer
   end
 
 
